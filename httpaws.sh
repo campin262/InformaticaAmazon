2717a0c3622ece46fb82b4e41645a58c46e802cf
aceherdz <aceherdz@gmail.com>
2016-05-28 19:09:32 -0500

    a

--- httpaws.sh
+++ httpaws.sh
@@ -40,7 +40,7 @@ case "$1" in
            echo cantidad de procesos en ejecucion $cant_procesos | tee -a $pathLog
            if [ "$cant_procesos" = "1" ]
            then
-              nvm use 4
+              /home/ec2-user/.nvm/nvm.sh use 4
               node $pathHttpdaws/main.js >> $pathLog  2>> $pathLog & 
            else
               echo | tee -a $pathLog
