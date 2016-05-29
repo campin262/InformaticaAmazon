7271e0b0e14fcd4a3745febf8b62d07113ef6d43
aceherdz <aceherdz@gmail.com>
2016-05-29 12:30:28 -0500

    ajustar error de sintaxis ...

--- log4Candy.js
+++ log4Candy.js
@@ -1,7 +1,7 @@
 function log() {
     var ahora = new Date().toLocaleString();
     var logMensaje = "";
-    logMensaje = logMensaje.concat("[",ahora,"] " arguments[0])
+    logMensaje = logMensaje.concat("[",ahora,"] ", arguments[0])
     for (var i = 1; i < arguments.length; i++) {
         logMensaje = logMensaje.replace("%s", arguments[i]);
     }
